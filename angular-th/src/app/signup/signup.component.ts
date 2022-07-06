27f2d483335fcc8817bd128d602b883fa2f113f6
sosotune <tslxg2019@gmail.com>
2022-07-06 14:07:37 -0400

    modified signup to not create duplicate users

--- angular-th/src/app/signup/signup.component.ts
+++ angular-th/src/app/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { catchError } from 'rxjs';
 import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
@@ -8,6 +9,15 @@ import { AuthenticationService } from '../services/authentication.service';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  newUser={
+    email:String,
+    firstName:String,
+    lastName:String,
+    password:String,
+    phone:String,
+    username:String,
+    zipcode:String
+  }
 
   constructor(private authService:AuthenticationService,
     private router: Router) { }
@@ -19,13 +29,25 @@ export class SignupComponent implements OnInit {
     if(value.password !== value.confirmPassword){
       return (alert("Passwords must match!!"));
     }
-    console.log(value)
-     this.authService.addUser(value)
+    let newUser={
+      email: value.email,
+      firstName:value.firstName,
+      lastName:value.lastName,
+      password:value.password,
+      phone:value.phone,
+      username:value.username,
+      zipcode:value.zipcode
+    }
+   
+     
+     
      this.authService.signup(value.email, value.password).subscribe(()=>{
-      alert("User successfully registered")
+      alert("User successfully registered");
+      this.authService.addUser(newUser)
       this.router.navigate(['']);
-    })
+    });
     
+   
 
   }
 }
