cd31f90f2b9648494f689a6d900a934987233db7
ah836798 <arlontus@hotmail.com>
2022-07-11 13:14:01 -0500

    old version

--- angular-th/src/app/app-routing.module.ts
+++ angular-th/src/app/app-routing.module.ts
@@ -4,19 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
  import { RegisterComponent } from './components/register/register.component';  */
 import {MatCheckboxModule} from '@angular/material/checkbox';
 
-const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'login' },
-/*  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }  */
 
-=======
 import { FullscreenOverlayContainer } from '@angular/cdk/overlay';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
-
 
 const routes: Routes = [
   {
--- angular-th/src/app/app.module.ts
+++ angular-th/src/app/app.module.ts
@@ -1,19 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
 import { MatMenuModule } from '@angular/material/menu';
 
 /* Routing */
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { ProductComponent } from './shared/product/product.component';
 import { LoginComponent } from './login/login.component';
@@ -27,46 +25,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { HotToastModule } from '@ngneat/hot-toast';
 
-
-/* Angular Material */
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
-/* FormsModule */
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 /* Angular Flex Layout */
 import { FlexLayoutModule } from "@angular/flex-layout";
 
-/* Components 
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';  */
-
-
 @NgModule({
   declarations: [
     AppComponent,
-
-  /*  LoginComponent,
-    RegisterComponent,   */
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    AngularMaterialModule,
-    ReactiveFormsModule,
-    FormsModule,
-    FlexLayoutModule
-
-
     UserProfileComponent,
-
     ProductComponent,
-      LoginComponent,
-      SignupComponent,
-      HomeComponent
+   LoginComponent,
+    SignupComponent,
+    HomeComponent
 
   ],
   imports: [
@@ -84,7 +56,11 @@ import { RegisterComponent } from './components/register/register.component';  *
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     BrowserAnimationsModule,
-    HotToastModule.forRoot()
+    HotToastModule.forRoot(),
+    AngularMaterialModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FlexLayoutModule
 
   ],
   providers: [],
