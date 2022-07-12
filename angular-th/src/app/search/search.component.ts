366036dfe21c5e8261071fddd01241156669d4ed
sosotune <tslxg2019@gmail.com>
2022-07-12 10:32:20 -0400

    so i can pull

--- angular-th/src/app/search/search.component.ts
+++ angular-th/src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewEncapsulation } from '@angular/core';
 
 
 @Component({
